34c74d40aadc45736649b3b1e028d8d166cd4cdd
LJ <81748770+elle-j@users.noreply.github.com>
2021-07-28 10:53:43 +0200

    Update canReadPartition

--- functions/canReadPartition.js
+++ functions/canReadPartition.js
@@ -1,22 +1,45 @@
 import { BSON } from 'realm';
 
 exports = async function(partition) {
+  if (!isValidPartition(partition)) {
+    console.warn('Invalid partition.');
+    return false;
+  }
+
   const db = context.services.get('mongodb-atlas').db('findourdevices');
   const realmUser = context.user;
 
-  if (realmUser.id === partition)
-    return true;
-  
-  try {
-    const userDoc = await db.collection('User').findOne({ _id: BSON.ObjectID(user.id) });
-    const isGroupMember = userDoc.groups
-      ? userDoc.groups.some(group => group.groupPartition === partition)
-      : false;
+  const partitionKey = getPartitionKey(partition);
+  const partitionValue = getPartitionValue(partition);
 
-    return isGroupMember;
-  }
-  catch (err) {
-    console.log('Error finding user: ', err.message);
-    return { error: { message: err.message || 'Could not find the user with the given ID.' } };
+  switch (partitionKey) {
+    case 'user':
+    case 'groupMembership':
+      return partitionValue === realmUser.id;
+    case 'group':
+      return await isGroupMember(db, BSON.ObjectID(realmUser.id), partition);
+    default:
+      console.warn('Unsupported partition key/prefix.');
+      return false;
   }
 };
+
+const isGroupMember = async (db, userId, groupPartition) => {
+  try {
+    // TODO: Create index on userId
+    const groupMembership = await db.collection('GroupMembership').findOne({ userId, groupPartition });
+
+    // The "!!" converts the value to a boolean (true if truthy, false if falsy)
+    return !!(groupMembership?.id);
+  }
+  catch (err) {
+    console.error('Error retrieving group membership.');
+    return false;
+  }
+}
+
+const isValidPartition = (partition) => partition.split('=').length === 2;
+
+const getPartitionKey = (partition) => partition.split('=')[0];
+
+const getPartitionValue = (partition) => partition.split('=')[1];
