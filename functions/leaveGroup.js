bced7b65da8f5b4bfcf4763adaa204f1abbc9836
LJ <81748770+elle-j@users.noreply.github.com>
2021-07-29 09:24:40 +0200

    Add leaveGroup function

---
+++ functions/leaveGroup.js
@@ -0,0 +1,33 @@
+exports = async function leaveGroup(groupId) {
+  if (!groupId)
+    return { error: { message: 'Please provide which group to leave.' }};
+  
+  const db = context.services.get('mongodb-atlas').db('findourdevices');
+  const realmUser = context.user;
+  const userId = BSON.ObjectID(realmUser.id);
+
+  try {
+    const groupDoc = await db.collection('Group').findOne({ _id: groupId });
+    if (!groupDoc?._id)
+      return { error: { message: 'The group does not exist.' }};
+
+    if (groupDoc.ownerId === userId)
+      return { error: { message: 'Group owners must remove the group in order to leave it.' }};
+
+    // Remove the group member from the group
+    await db.collection('Group').updateOne(
+      { _id: groupId },
+      { $pull: { members: { userId } } }
+    );
+
+    // Remove the user's group membership document
+    await db.collection('User').updateOne(
+      { _id: userId },
+      { $pull: { groups: { groupId } } }
+    );
+  }
+  catch (err) {
+    console.error('Error leaving the group: ', err.message);
+    return { error: { message: err.message || 'There was an error leaving the group.' } };
+  }
+};
