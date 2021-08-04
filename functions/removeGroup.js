3a86e586303ffd6cc603159766c9e613dd1ce869
José Martins <zepedromm1998@gmail.com>
2021-08-04 17:54:57 +0100

    Fixed removeGroup function not using groupId as BSON.ObjectId

--- functions/removeGroup.js
+++ functions/removeGroup.js
@@ -5,20 +5,23 @@ exports = async function removeGroup(groupId) {
   const db = context.services.get('mongodb-atlas').db('findourdevices');
   const realmUser = context.user;
   const userId = BSON.ObjectId(realmUser.id);
+  groupId = BSON.ObjectId(groupId);
 
   try {
     const groupDoc = await db.collection('Group').findOne({ _id: groupId });
     if (!groupDoc?._id)
       return { error: { message: 'The group does not exist.' } };
 
-    if (groupDoc.ownerId !== userId)
+    // Stringify the ObjectIds when comparing as the references themselves may differ
+    if (groupDoc.ownerId.toString() !== userId.toString())
       return { error: { message: 'Only owners are allowed to remove a group.' } };
-
+  
     await db.collection('Group').deleteOne({ _id: groupId });
     
     // After deleting the group, we also need to remove all GroupMemberships
     // from the 'groups' array of each User
     const memberIds = groupDoc.members.map(member => member.userId);
+    
     if (memberIds.length > 0) {
       await db.collection('User').updateMany(
         { _id: { $in: memberIds } },
