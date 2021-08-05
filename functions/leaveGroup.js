cdffca7aeb0e89c7d94a90bfd90241de2306db0d
LJ <81748770+elle-j@users.noreply.github.com>
2021-08-05 09:42:19 +0200

    Convert groupId arg into ObjectId in leaveGroup. Modify comments

--- functions/leaveGroup.js
+++ functions/leaveGroup.js
@@ -5,26 +5,30 @@ exports = async function leaveGroup(groupId) {
   const db = context.services.get('mongodb-atlas').db('findourdevices');
   const realmUser = context.user;
   const userId = BSON.ObjectId(realmUser.id);
+  groupId = BSON.ObjectId(groupId);
 
   try {
     const groupDoc = await db.collection('Group').findOne({ _id: groupId });
     if (!groupDoc?._id)
       return { error: { message: 'The group does not exist.' }};
 
-    if (groupDoc.ownerId === userId)
+      // Stringify the ObjectIds when comparing as the references themselves may differ
+    if (groupDoc.ownerId.toString() === userId.toString())
       return { error: { message: 'Group owners must remove the group in order to leave it.' }};
 
-    // Remove the group member from the group
+    // Remove the GroupMember from the Group's 'members' array
     await db.collection('Group').updateOne(
       { _id: groupId },
       { $pull: { members: { userId } } }
     );
 
-    // Remove the user's group membership document
+    // Remove the User's GroupMembership from it's 'groups' array
     await db.collection('User').updateOne(
       { _id: userId },
       { $pull: { groups: { groupId } } }
     );
+
+    return { success: true };
   }
   catch (err) {
     console.error('Error leaving the group: ', err.message);
